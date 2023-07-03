b935d9b4d26074973453904b1e152c467fa9f8a9
Papika0 <72442767+Papika0@users.noreply.github.com>
2023-07-04 00:44:08 +0400

    feat -  user factory add thumbnail, update thumbnail value

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-use App\Models\Like;
 use App\Models\User;
 use App\Models\Genre;
 use App\Models\Movie;
@@ -21,28 +20,33 @@ class DatabaseSeeder extends Seeder
 	{
 		$users = User::factory(3)->create();
 
+		$genres = Genre::factory(15)->create();
+
+		$randomUsers = User::factory(25)->create();
+
+		$genre = $genres->random();
+
 		foreach ($users as $user) {
 			$movies = Movie::factory(5)->create(['user_id' => $user->id]);
 
 			foreach ($movies as $movie) {
-				Quote::factory(5)->create([
+				$quotes = Quote::factory(5)->create([
 					'user_id'  => $user->id,
 					'movie_id' => $movie->id,
 				]);
 
-				$genres = Genre::factory(3)->create();
+				$quote = $quotes->random();
 
-				$movie->genres()->attach($genres->pluck('id'));
+				$movie->genres()->attach($genre->pluck('id'));
 
-				$likes = Like::factory(10)->create([
-					'user_id'  => $user->id,
-					'quote_id' => $movie->quotes->pluck('id')->random(),
-				]);
+				foreach ($randomUsers as $randomUser) {
+					$quote->likes()->attach($randomUser->id);
 
-				$comments = Comment::factory(10)->create([
-					'user_id'  => $user->id,
-					'quote_id' => $movie->quotes->pluck('id')->random(),
-				]);
+					$comments = Comment::factory(1)->create([
+						'user_id'  => $randomUser->id,
+						'quote_id' => $movie->quotes->pluck('id')->random(),
+					]);
+				}
 			}
 		}
 	}
