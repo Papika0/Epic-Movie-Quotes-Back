3496722539a87ea6dd630a766950f5c0108f3daa
Papika0 <72442767+Papika0@users.noreply.github.com>
2023-07-09 00:03:44 +0400

    feat - add private channel route

---
+++ app/Broadcasting/NotificationsChannel.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Broadcasting;
+
+use App\Models\User;
+
+class NotificationsChannel
+{
+	/**
+	 * Create a new channel instance.
+	 */
+	public function __construct()
+	{
+	}
+
+	/**
+	 * Authenticate the user's access to the channel.
+	 */
+	public function join(User $user, $userId): array|bool
+	{
+		return (int)$user->id === (int)$userId;
+	}
+}
