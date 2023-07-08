d18841f28d25cec3999173926b3c01e82f6353f2
Papika0 <72442767+Papika0@users.noreply.github.com>
2023-07-09 00:04:48 +0400

    feat - add 3 events comment/like/notification

---
+++ app/Events/CommentAdded.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class CommentAdded implements ShouldBroadcast
+{
+	use Dispatchable, InteractsWithSockets, SerializesModels;
+
+	public $message;
+
+	/**
+	 * Create a new event instance.
+	 */
+	public function __construct($message)
+	{
+		$this->message = $message;
+	}
+
+	/**
+	 * Get the channels the event should broadcast on.
+	 *
+	 * @return array<int, \Illuminate\Broadcasting\Channel>
+	 */
+	public function broadcastOn(): array
+	{
+		return [
+			new Channel('comments'),
+		];
+	}
+}
