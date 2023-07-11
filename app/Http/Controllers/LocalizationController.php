164b20c52cb90715c9f93bf36b92ff5dc073e061
Papika0 <72442767+Papika0@users.noreply.github.com>
2023-07-11 22:18:26 +0400

    fix - locale change route middleware

--- app/Http/Controllers/LocalizationController.php
+++ app/Http/Controllers/LocalizationController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\App;
 
 class LocalizationController extends Controller
 {
-	public function setLanguage(string $locale)
+	public function setLanguage(string $locale): JsonResponse
 	{
 		App::setLocale($locale);
 		session()->put('locale', $locale);
